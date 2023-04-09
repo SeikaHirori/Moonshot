ee9f3f76968552c014945e7f9376b8f854fb7728
SeikaHirori <SeikaHirori@outlook.com>
2023-04-09 15:01:38 -0700

    added maximum parameter to adaptive parameter

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -155,7 +155,7 @@ struct lesson_section_1_5: View {
 struct lesson_section_1_6: View {
     
     let layout: [GridItem] = [
-        GridItem(.adaptive(minimum: 80)),
+        GridItem(.adaptive(minimum: 80, maximum: 120)),
     ]
     
     var body: some View {
