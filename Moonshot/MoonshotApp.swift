a1b9d7d6240c9617835172ca568c900bc79a31a3
SeikaHirori <SeikaHirori@outlook.com>
2023-04-09 08:44:03 -0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Seika Hirori on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
