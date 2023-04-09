45631bbdbd31848cff25221866daea035aed2667
SeikaHirori <SeikaHirori@outlook.com>
2023-04-09 10:06:38 -0700

    Loaded image, but it current takes up AND extends over the screen boundries.

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,18 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        lesson_part_1()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct lesson_part_1: View {
+    var body: some View {
+       return VStack{
+        
+           Text("hello world")
+           Image("Example")
+           
+       }
     }
 }
