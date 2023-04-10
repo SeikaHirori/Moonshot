0a43d1945d3be9e6a3954863e1baa02dbe168f67
SeikaHirori <SeikaHirori@outlook.com>
2023-04-10 09:24:33 -0700

    Slowly transforming func decode() into being a generic; current need to have it conform to Codablw

--- Moonshot/Mission.swift
+++ Moonshot/Mission.swift
@@ -8,14 +8,15 @@
 import Foundation
 
 struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    
     let id: Int
     let launchDate:String?
     let crew: [CrewRole]
     let description: String
-}
-
-struct CrewRole: Codable {
-    let name: String
-    let role: String
 }
 
