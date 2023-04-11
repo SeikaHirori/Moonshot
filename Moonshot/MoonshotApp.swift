7741de83d2a920569dbac64d842f47f579422811
SeikaHirori <SeikaHirori@outlook.com>
2023-04-11 11:22:33 -0700

    Restored missing file that broke app

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,18 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Seika Hirori on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
